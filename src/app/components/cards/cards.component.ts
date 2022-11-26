2c652ca79d61876d77bd84f7ce3f51d00542c69c
ThiagoMASS <116855408+ThiagoMASS@users.noreply.github.com>
2022-11-25 20:00:29 -0400

    Card component

---
+++ src/app/components/cards/cards.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+
+
+export interface CardInterface {
+  title: string;
+  description: string;
+  button: string;
+}
+
+@Component({
+  selector: 'app-cards',
+  templateUrl: './cards.component.html',
+  styleUrls: ['./cards.component.css']
+})
+
+
+
+export class CardsComponent implements OnInit{
+
+
+  public listOfCards: Array<CardInterface> = [
+    {
+    "title": "Curso de javascript basico",
+    "description":"Curso de javascript para iniciantes",
+    "button": "Botao do Thiago"
+    },
+    {
+      "title": "Curso de Banco da dados basico",
+      "description":"Curso de javascript para iniciantes",
+      "button": "Botao do Thiago"
+    },
+    {
+      "title": "Curso de Data science basico",
+      "description":"Curso de javascript para iniciantes",
+      "button": "Botao do Thiago"
+    },
+];
+
+
+  private constructor() {
+
+  }
+
+
+  ngOnInit(): void {
+    // this.makeCards();
+    console.log(this.listOfCards);
+  }
+
+  public clickHere(desc: string):void{
+    console.log(desc);
+  }
+
+
+}
